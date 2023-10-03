4cb8950abf008dc5db755750554bb9b0b122625c
Neil De La Cruz <studyingrhystic@gmail.com>
2023-10-02 21:10:13 -0400

    further cleanup of DealTiles

--- Hand.cs
+++ Hand.cs
@@ -7,6 +7,20 @@ namespace Mahjong
 {
     public class Hand
     {
+        public List<Tile> DealHand(List<Tile> tiles, List<Player> players)
+        {
+            int j = 4;
+            for (int i = 0; i < 13; i += 4)
+            {
+                if (i == 12)
+                {
+                    j = 13;
+                }
+                tiles = DealTiles(tiles, players, i, j);
+                j += 4;
+            }
+            return tiles;
+        }
         public List<Tile> DealTiles(List<Tile> tiles, List<Player> players, int startingIndex, int endingIndex)
         {
             bool isDealer = false;
@@ -16,36 +30,19 @@ namespace Mahjong
                 //an initial hand in mahjong is drawn in sets of 4 per player
                 //then you draw 2 if dealer, and 1 if not a dealer.
                 int playerTileIndex = startingIndex;
-                while (playerTileIndex < 13)
+                while (playerTileIndex < endingIndex)
                 {
-                    if (playerTileIndex == 12 && isDealer)
-                    {
+                    player.Hand.Add(tiles[playerTileIndex]);
+                    tempTiles.Remove(tiles[playerTileIndex]);
+                    playerTileIndex++;
+                }
+                if (playerTileIndex == 13 && isDealer)
+                {
 
-                        player.Hand.Add(tiles[playerTileIndex]);
-                        player.Hand.Add(tiles[playerTileIndex + 1]);
-                        tempTiles.Remove(tiles[playerTileIndex]);
-                        tempTiles.Remove(tiles[playerTileIndex + 1]);
-                        tiles = tempTiles;
-                        break;
-                    }
-                    else
-                    {
-                        player.Hand.Add(tiles[playerTileIndex]);
-                        tempTiles.Remove(tiles[playerTileIndex]);
-                        playerTileIndex++;
-                    }
+                    player.Hand.Add(tiles[playerTileIndex]);
+                    tempTiles.Remove(tiles[playerTileIndex]);
                 }
                 tiles = tempTiles;
-            }
-            return tiles;
-        }
-        public List<Tile> DealHand(List<Tile> tiles, List<Player> players)
-        {
-            int j = 4;
-            for (int i = 0; i < 13; i += 4)
-            {
-                tiles = DealTiles(tiles, players, i, j);
-                j += 4;
             }
             return tiles;
         }
